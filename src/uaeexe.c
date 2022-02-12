76276a06f50745fbcb3733e8049f5a86be823dc5
threader <michael.heltne@gmail.com>
2022-02-12 13:22:11 +0100

    modified:   uaeexe.c

--- src/uaeexe.c
+++ src/uaeexe.c
@@ -18,11 +18,7 @@
 
 static struct uae_xcmd *first = NULL;
 static struct uae_xcmd *last  = NULL;
-<<<<<<< HEAD
-static char running = 0;
-=======
 static TCHAR running = 0;
->>>>>>> p-uae/v2.1.0
 static uae_u32 uaeexe_server (TrapContext *context) REGPARAM;
 
 /*
@@ -34,11 +30,7 @@ void uaeexe_install (void)
 
     loop = here ();
     org (UAEEXE_ORG);
-<<<<<<< HEAD
-    calltrap (deftrap (uaeexe_server));
-=======
 	calltrap (deftrapres (uaeexe_server, 0, "uaeexe_server"));
->>>>>>> p-uae/v2.1.0
     dw (RTS);
     org (loop);
 }
@@ -52,29 +44,11 @@ void uaeexe_install (void)
  * to launch the command asynchronously. Please note also that the
  * remote cli works better if you've got the fifo-handler installed.
  */
-<<<<<<< HEAD
-int uaeexe (const char *cmd)
-=======
 int uaeexe (const TCHAR *cmd)
->>>>>>> p-uae/v2.1.0
 {
     struct uae_xcmd *nw;
 
     if (!running)
-<<<<<<< HEAD
-	goto NORUN;
-
-    nw = (struct uae_xcmd *) malloc (sizeof *nw);
-    if (!nw)
-	goto NOMEM;
-    nw->cmd = (char *) malloc (strlen (cmd) + 1);
-    if (!nw->cmd) {
-	free (nw);
-	goto NOMEM;
-    }
-
-    strcpy (nw->cmd, cmd);
-=======
 		goto NORUN;
 
 	nw = xmalloc (struct uae_xcmd, 1);
@@ -87,25 +61,10 @@ int uaeexe (const TCHAR *cmd)
     }
 
 	_tcscpy (nw->cmd, cmd);
->>>>>>> p-uae/v2.1.0
     nw->prev = last;
     nw->next = NULL;
 
     if (!first)
-<<<<<<< HEAD
-	first      = nw;
-    if (last) {
-	last->next = nw;
-	last       = nw;
-    } else
-	last       = nw;
-
-    return UAEEXE_OK;
-  NOMEM:
-    return UAEEXE_NOMEM;
-  NORUN:
-    write_log ("Remote cli is not running.\n");
-=======
 		first      = nw;
     if (last) {
 		last->next = nw;
@@ -118,22 +77,12 @@ NOMEM:
     return UAEEXE_NOMEM;
 NORUN:
 	write_log ("Remote cli is not running.\n");
->>>>>>> p-uae/v2.1.0
     return UAEEXE_NOTRUNNING;
 }
 
 /*
  * returns next command to be executed
  */
-<<<<<<< HEAD
-static char *get_cmd (void)
-{
-    struct uae_xcmd *cmd;
-    char *s;
-
-    if (!first)
-	return NULL;
-=======
 static TCHAR *get_cmd (void)
 {
     struct uae_xcmd *cmd;
@@ -141,16 +90,11 @@ static TCHAR *get_cmd (void)
 
     if (!first)
 		return NULL;
->>>>>>> p-uae/v2.1.0
     s = first->cmd;
     cmd = first;
     first = first->next;
     if (!first)
-<<<<<<< HEAD
-	last = NULL;
-=======
 		last = NULL;
->>>>>>> p-uae/v2.1.0
     free (cmd);
     return s;
 }
@@ -158,19 +102,7 @@ static TCHAR *get_cmd (void)
 /*
  * helper function
  */
-<<<<<<< HEAD
 #define ARG(x) (get_long (m68k_areg (&context->regs, 7) + 4*(x+1)))
-static uae_u32 REGPARAM2 uaeexe_server (TrapContext *context)
-{
-    int len;
-    char *cmd;
-    char *dst;
-
-    if (ARG(0) && !running) {
-	running = 1;
-	write_log ("Remote CLI started.\n");
-=======
-#define ARG(x) (get_long (m68k_areg (regs, 7) + 4 * (x + 1)))
 static uae_u32 REGPARAM2 uaeexe_server (TrapContext *context)
 {
     int len;
@@ -180,34 +112,20 @@ static uae_u32 REGPARAM2 uaeexe_server (TrapContext *context)
     if (ARG(0) && !running) {
 		running = 1;
 		write_log ("Remote CLI started.\n");
->>>>>>> p-uae/v2.1.0
     }
 
     cmd = get_cmd ();
     if (!cmd)
-<<<<<<< HEAD
-	return 0;
-    if (!ARG(0)) {
-	running = 0;
-	return 0;
-=======
 		return 0;
     if (!ARG(0)) {
 		running = 0;
 		return 0;
->>>>>>> p-uae/v2.1.0
     }
 
     dst = (char *) get_real_address (ARG (0));
     len = ARG (1);
-<<<<<<< HEAD
-    strncpy (dst, cmd, len);
-    write_log ("Sending '%s' to remote cli\n", cmd);
-    free (cmd);
-=======
 	strncpy (dst, cmd, len);
 	write_log ("Sending '%s' to remote cli\n", cmd);
 	xfree (cmd);
->>>>>>> p-uae/v2.1.0
     return ARG (0);
 }
