1b0b15d6958433b032530dc5f87d21606c5d8ab2
threader <michael.heltne@gmail.com>
2022-10-08 09:40:29 +0200

    audio.h

--- src/include/audio.h
+++ src/include/audio.h
@@ -26,7 +26,7 @@ extern void audio_reset (void);
 extern void update_audio (void);
 extern void schedule_audio (void);
 extern void audio_evhandler (void);
-extern void audio_hsync (int);
+extern void audio_hsync (unsigned int);
 extern void audio_update_adkmasks (void);
 extern void audio_update_irq (uae_u16);
 extern void update_sound (unsigned int freq, int longframe, int linetoggle);
